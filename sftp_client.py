385690763d974014881c5ecf90d58a19dd690986
Devquercus <devquercus@gmail.com>
2020-12-01 15:54:00 +0200

    Update the description

--- sftp_client.py
+++ sftp_client.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 '''
-This script uses the paramiko module to make a connection to Draxis sftp server
-where the Africultures services are stored
-You need to set the path for the id_rsa file, the keypass for that file 
+This script uses the paramiko module to make a connection to an sftp server. 
+
+You need to set the path for the id_rsa file and the keypass for that file 
+
 If you want to upload any files in the server you must define the local and remote paths.
+
 TODO:
 - Make a function to upload the files and check if the defined remote path exists or not and create the appropriate path 
 if needed
